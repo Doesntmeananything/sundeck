08be87a18f576ab5b566eb3c71a98bb42f5fc1ac
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-05-11 21:05:49 +0300

    Add basic header with color mode toggle

---
+++ shared/Header.tsx
@@ -0,0 +1,34 @@
+import { Flex, Heading, IconButton, useColorMode } from "@chakra-ui/core";
+
+export const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      h={16}
+      px={4}
+      borderBottomWidth="1px"
+    >
+      <Heading as="h1" size="lg">
+        Sundeck
+      </Heading>
+
+      <Flex>
+        <IconButton
+          aria-label={`Switch to ${
+            colorMode === "light" ? "dark" : "light"
+          } mode`}
+          variant="ghost"
+          fontSize="20px"
+          onClick={toggleColorMode}
+          icon={colorMode === "light" ? "moon" : "sun"}
+        >
+          Toggle {colorMode === "light" ? "Dark" : "Light"}
+        </IconButton>
+      </Flex>
+    </Flex>
+  );
+};
