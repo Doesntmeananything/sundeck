60bfef292ac0e22ab71b68798e0777416e7f14dc
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-05-11 21:05:14 +0300

    Fix css reset overwriting color mode provider values

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,8 +18,8 @@ const customTheme = produce(theme, (newTheme) => {
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={customTheme}>
-      <CSSReset />
       <ColorModeProvider>
+        <CSSReset />
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap"
