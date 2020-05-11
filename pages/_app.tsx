f8ecf5815963848bcf72bbe6d2172e3144836ff0
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-05-11 20:21:23 +0300

    Add a custom font to the App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,35 @@
+import {
+  CSSReset,
+  ColorModeProvider,
+  ThemeProvider,
+  theme
+} from "@chakra-ui/core";
+import produce, { enableAllPlugins } from "immer";
 import { AppProps } from "next/app";
-import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
+enableAllPlugins();
+
+const customTheme = produce(theme, (newTheme) => {
+  newTheme.fonts.heading =
+    'Philosopher, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+});
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={customTheme}>
       <CSSReset />
       <ColorModeProvider>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <Component {...pageProps} />
       </ColorModeProvider>
     </ThemeProvider>
   );
-}
+};
 
 export default MyApp;
